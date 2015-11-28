112b6002d5aad9b452bad94fdc9e734ae25ffdc8
Brett Suwyn <brett@suwyn.com>
2015-11-28 10:07:46 -0800

    Adds mvp epic spec

--- spec/export_command_spec.rb
+++ spec/export_command_spec.rb
@@ -3,15 +3,47 @@ require 'export_command'
 describe Outhaul::ExportCommand do
 
   describe '#run' do
-    context 'with a valid file path' do
-      let(:file) { './spec/fixtures/empty.html' }
-      let(:command) { Outhaul::ExportCommand.new }
+    let(:file) { '/dev/null' }
+    let(:options) { {api_token: '0000', project_id: '0000' } }
+    let(:export_command) { Outhaul::ExportCommand.new options }
+    let(:epic_endpoint) { TrackerApi::Endpoints::Epic }
+    let(:epic_resource) { TrackerApi::Resources::Epic }
+    let(:html) { '' }
 
-      subject { command.run file }
+    subject(:command) { export_command.run file }
 
-      it 'returns true' do
-        expect(subject).to be true
+    before do
+      allow_any_instance_of(epic_endpoint).to receive(:create) do |instance, object, project_id, params|
+        epic.name = params[:name]
+        epic
       end
+
+      allow_any_instance_of(epic_resource).to receive(:save)
+
+      allow(IO).to receive(:read).and_return(html)
+    end
+
+    context 'successful execution' do
+      it 'returns true' do
+        expect(command).to be true
+      end
+    end
+
+    context 'with a Document containing a H1' do
+      let(:epic) { epic_resource.new }
+      let(:epic_name) { 'This is an Epic Name' }
+      let(:html) { "<h1>#{epic_name}</h1>" }
+
+      subject(:result) do
+        command
+        epic
+      end
+
+      it 'creates an Epic with a name matching the H1 text' do
+        expect_any_instance_of(epic_endpoint).to receive(:create)
+        expect(result.name).to eq epic_name
+      end
+
     end
   end
 
